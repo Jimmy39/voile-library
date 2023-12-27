2a36c2bfd76c7eb311c86bc47d3c34037be3c6ae
Jimmy39 <wanghanyu2000@126.com>
2023-12-27 23:32:35 +0800

    add voile_ReturnHardwareUnsupportedError
    add function voile_ReturnHardwareUnsupportedError

--- library/voile_common.h
+++ library/voile_common.h
@@ -2,27 +2,30 @@
  * @file voile_common.h
  * @author Jimmy Wang (wanghanyu2000@126.com)
  * @brief Common denpence for voile library
- * 
+ *
  */
 #ifndef __VOILE_COMMON_H_
 #define __VOILE_COMMON_H_
 
-
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #define bool _Bool
 
 /**
  * @brief Return value meaning
- * 
+ *
  */
-typedef enum{
+typedef enum {
     success = 0,
     failure = 1,
     inputRangeError = 2,
     hardwareUnsupportedError = 3,
 } voile_status_t;
 
+static inline voile_status_t voile_ReturnHardwareUnsupportedError(const void*, ...)
+{
+    return hardwareUnsupportedError;
+}
 
 #endif // !__VOILE_COMMON_H_
